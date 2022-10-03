175a469d06602a11e3ce30b481f9de9b51e8e4e4
Uziel Trujillo <uziel.trujillo.mx@cloudappi.net>
2022-10-03 14:52:51 -0500

    Agregando foreach 	modified:   unidadI/05_objetos.js

--- unidadI/05_objetos.js
+++ unidadI/05_objetos.js
@@ -12,4 +12,24 @@ const diana = {
 
 const directorio = [juan, diana]
 
-// console.log(directorio)
+// console.log(directorio[0].correo)
+
+// for( i = 0; i < directorio.length; i++ ){
+//   let nombreCompleto = directorio[i].nombre 
+//                         +" "+ directorio[i].apellidos
+//   console.log('Nombre completo: ', nombreCompleto)
+// }
+
+// directorio.forEach( (item, index) => {
+//   console.log(`${index} Nombre completo: ${item.nombre} ${item.apellidos}`)
+// } )
+
+// directorio.forEach( item => {
+//   console.log(`Nombre completo: ${item.nombre} ${item.apellidos}`)
+// } )
+
+const new_directory = directorio.map( element => ({
+  nombreCompleto: element.nombre +' '+ element.apellidos
+}) )
+
+console.log( new_directory )
